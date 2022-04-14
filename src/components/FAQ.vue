df7a5fc7b56ef65256fcb127575ff7a3fc236662
Faust Alexander <faustalexander@protonmail.com>
2022-04-14 00:12:07 -0500

    Implemented FAQ section.

---
+++ src/components/FAQ.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h2>What is Discussions?</h2>
+    <p>A whole new atmosphere of freedom. Free to say, to do, to transact and to build together. Built with intensity, built with tenacity and built with freedom and liberty for a better future.</p>
+
+    <p>Discussions provides you with the tools to build a community around the world to fulfill your collective vision. It is a community building engine that helps you turn your vision into the future. Built with blockchain, and connected with the world, with Discussions you can leverage your Twitter, Telegram, Facebook and LinkedIn communities to build a new world.</p>
+
+    <p>ATMOS tokens are the underlying technology that power the platform and the smart-contracts. It can be utilized to pay for locked content, advertisements, fund transactions, visual and user experience enhancements.</p>
+
+    <p>ATMOS can be obtained via the exchanges ProBit or NewDex.</p>
+
+    <p>In addition to ATMOS, Discussions has provided a way for users to obtain tokens from other communities via instant swaps.</p>
+
+    <p>The Novusphere is a nimble team building open software and leveraging emerging technology to unite blockchains and put users in control of the next wave of connected applications.</p>
+
+    <h3>How does the site work?</h3>
+    <p>Discussions makes use of a blockchain based architecture to share content. Nodes will be able to write content and descentralize it so the site is resistant to censorship by means of the Telos blockchain.</p>
+
+    <p>Being on the blockchain means any post once made remains there forever, which ensures that it can't be censored since anyone that knows its location can retrieve it again.</p>
+
+    <h3>What are tags?</h3>
+    <p>Tags allow to classify content so it becomes more easily discovered. They work by adding the "#" sign, for example #cats . This makes sure that every site connecting to the blockchain knows the topic is related to the cats category and any community can draw them from it.</p>
+
+    <p>Tags aren't controlled by anyone which further ensures they can't be censored. Anyone can post using any tags they want</p>
+
+    <h3>What are communities?</h3>
+    <p>Unlike tags, communities are user curated which allows community managers to eliminate mistagged content or malicious posts from the communities.</p>
+
+    <p>To prevent abuse, communities can be forked so clones of them can be made with exactly the same content and hiding content in them doesn't affect the tags.</p>
+
+    <p>Communities can be referenced using the "*" sign. For example by posting *cats the link will take users directly to the cats community which is curated by a team to ensure all posts remain within that community standards. It is separate from the cats tag which is unmoderated and has every post ever made with the tag, even unrelated posts. This ensures users can have curated content in a community to suit their specific needs and uncurated content in tags to ensure no censorship while also having the ability to make their own communities if they so wish.</p>
+
+    <h3>What is tipping?</h3>
+    <p>Being a Blockchain based platform means that users can trade cryptocurrency in the platform. Particularly good posts can be rewarded by users through the tipping mechanism.</p>
+
+    <p>When a user tips, he transfers a certain amount of tokens to the post, increasing visibility (thus its subjective worth) while also rewarding the poster for contributing valuable material. You can consult how much cryptocurrency you have in the wallet menu.</p>
+
+    <p>Currently the site allows to tip using ATMOS and TELOS, but more cryptocurrency is incoming!</p>
+
+    <h3>I have a feature I'd like to request</h3>
+    <p>You can make a request using the DAO or tagging a post with #DiscussionsWishList tag</p>
+
+    <h3>Currently active Discussions nodes</h3>
+    <p>Discussions ensures it can't be censored thanks to its growing number of nodes. You can access them or even host your own with its own rules and features.</p>
+    <ul>
+      <li><a href="https://discussions.app/">https://discussions.app/</a> - The main site and where the project is currently hosted</li>
+      <li><a href="https://beta.discussions.app/">https://beta.discussions.app/</a> - The bleeding edge site where the newest features are tested and are available so you can try them out!</li>
+      <li><a href="https://cooming.live/">https://cooming.live/</a> - A joke domain by @FaustAlexander , contributions are made and is pretty anime friendly</li>
+    </ul>
+
+  </div>
+</template>
+<script>
+
+export default {
+  name: "FAQ",
+}
+</script>
